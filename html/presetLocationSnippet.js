a943c9e68e90e9e26daefe07b2ce90b734a8a814
Sunny Mui <sunnycmui@gmail.com>
2021-04-03 14:58:13 -0700

    deprecate a bunch of the dynamic info window opening logic and switch to opening corresponding project page in new window, refactor the preset marker centering/zoom/hoverization logic to be based on post message api, add wix post message snippet version for preset marker functionality, add options to popup and hoverize functions to have optional popup or cta showing

--- html/presetLocationSnippet.js
+++ html/presetLocationSnippet.js
@@ -18,3 +18,29 @@ document.addEventListener('message', (e) => {
         mapApp.postMessage(formattedPayload, CONSTANTS.authorized_parent_domain);
     }
 });
+
+
+// the wix velo version of the snippet
+
+$w.onReady(function () {
+	// this should be the id of the html element containing the map
+	const map = $w('#html1');
+
+	// wait for the map iframe component to signal it's ready to receive
+	map.onMessage( (e) => {
+		const { data, origin } = e;
+		// this should be the id of the text element containing the Project Name to search,
+		// match, and get the corresponding project from the mapbox dataset
+		const projectName = $w('#text51').text;
+
+		if (data === 'ready') {
+			const payload = {
+				presetMarker: projectName
+			};
+
+			const formattedPayload = JSON.stringify(payload);
+
+			map.postMessage(formattedPayload);
+		}
+	});
+});
